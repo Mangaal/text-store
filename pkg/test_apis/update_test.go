11feb31a804323d42c2375fff19456a1a4073ead
Mangaal <angommeeteimangaal@gmail.com>
2023-08-10 00:31:26 +0530

    update unit test for fail update

--- pkg/test_apis/update_test.go
+++ pkg/test_apis/update_test.go
@@ -27,3 +27,20 @@ func TestUpdateFileContent(t *testing.T) {
 	assert.Equal(t, http.StatusOK, resp.Code)
 
 }
+
+func TestUpdateFileContent_Fail(t *testing.T) {
+	gin.SetMode(gin.TestMode)
+	router := gin.Default()
+
+	router.POST("/apis/file/:newname/:oldname", apis.UpdateFileContent)
+
+	oldName := "ping.txt"
+	newName := "newfile.txt"
+
+	req := httptest.NewRequest("POST", fmt.Sprintf("/apis/file/%s/%s", newName, oldName), nil)
+	resp := httptest.NewRecorder()
+	router.ServeHTTP(resp, req)
+
+	assert.Equal(t, http.StatusInternalServerError, resp.Code)
+
+}
